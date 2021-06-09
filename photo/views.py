f52fff554fbdadb9f31306c331c709c5f279a395
ghk0409 <ghk0409@gmail.com>
2021-06-09 22:38:05 +0900

    로그인 권한 설정 및 Detail View 수정

--- photo/views.py
+++ photo/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from .models import Photo
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.detail import DetailView
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # 함수형 뷰로 photo_list 뷰 만들기
 # 함수형 뷰는 모든 기능을 직접 처리해야 함(상속 받지 못해서)
+# 함수형 뷰는 Decorator 사용
+@login_required()
 def photo_list(request):
     photos = Photo.objects.all()
     return render(request, 'photo/list.html', {'photos':photos})
 
+# 클래스형 뷰는 Mixin을 첫 번째로 상속받도록
 # 제네릭 뷰로 PhotoUploadView 만들기
-class PhotoUploadView(CreateView):
+class PhotoUploadView(LoginRequiredMixin, CreateView):
     model = Photo
     fields = ['photo', 'text']
     template_name = 'photo/upload.html'
@@ -30,13 +36,17 @@ class PhotoUploadView(CreateView):
             return self.render_to_response({'form':form})
 
 # 제네릭 뷰로 PhotoDeleteView 만들기
-class PhotoDeleteView(DeleteView):
+class PhotoDeleteView(LoginRequiredMixin, DeleteView):
     model = Photo
     success_url = '/'
     template_name = 'photo/delete.html'
 
 # 제네릭 뷰로 PhotoUpdateView 만들기
-class PhotoUpdateView(UpdateView):
+class PhotoUpdateView(LoginRequiredMixin, UpdateView):
     model = Photo
     fields = ['photo', 'text']
     template_name = 'photo/update.html'
+
+class PhotoDetailView(LoginRequiredMixin, DetailView):
+    model = Photo
+    template_name = 'photo/detail.html'
